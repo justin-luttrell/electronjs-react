4be978de4bebc2a616611e630c3fc5695b697947
justin-luttrell <jluttrell@xometry.com>
2020-04-24 17:40:34 -0400

    Add app icon in dev mode

--- public/electron.js
+++ public/electron.js
@@ -3,13 +3,12 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow(isDev ? {width: 900, height: 680, icon: path.join(__dirname, '../assets/icon.png')} : {width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
